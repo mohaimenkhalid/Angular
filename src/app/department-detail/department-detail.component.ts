46c656d3e31497a90c56c712fca7874b4320eb2b
mohaimenkhalid <mohaimen707@gmail.com>
2021-03-08 16:19:02 +0600

    paramMap Observable

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router, ParamMap } from '@angular/router'
 @Component({
   selector: 'app-department-detail',
   template: `
     <p>
       department-detail works!ID - {{ departmentId }}
     </p>
+    <button (click)="goPrev()">Previous</button>
+    <button (click)="goNext()">Next</button>
   `,
   styles: [
   ]
@@ -13,10 +15,23 @@ import { ActivatedRoute } from '@angular/router'
 export class DepartmentDetailComponent implements OnInit {
 
   public departmentId:any;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.departmentId = parseInt(this.route.snapshot.paramMap.get('id')!);
+    //this.departmentId = parseInt(this.route.snapshot.paramMap.get('id')!);
+      this.route.paramMap.subscribe((params: ParamMap) => {
+        this.departmentId = parseInt(params.get('id')!)
+    });
+  }
+
+  goNext() {
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
+  }
+
+  goPrev() {
+    let prevId = this.departmentId - 1;
+    this.router.navigate(['/departments', prevId]);
   }
 
 }
